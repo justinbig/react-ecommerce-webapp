70a64f38a30be4be97fe731f2ebeb726d0781180
justin big <justinbig2003@gmail.com>
2019-08-08 10:00:41 +0200

    adding pageompont

--- src/App.js
+++ src/App.js
@@ -1,50 +1,24 @@
 import React,{Component} from 'react';
 
-import {CardList} from "./components/card-list/card-list.component";
+import HomePage from "./components/homepage.component";
 
-import {SearchBox} from "./components/searchBox/search-box.component";
 
 import './App.css';
 
 
 class App extends Component {
-
-   state = {
-     monsters : [] ,
-     searchField : ""
-      
-     
-  } 
-
-  onSearchHandler = (e) => {
-     this.setState({searchField : e.target.value})
-  }
-
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-    .then(res => res.json())
-    .then(users => this.setState({monsters : users}))
-  }
-   render () {
-     const {monsters,searchField} = this.state;
-    const filteredMonster = monsters.filter(monster => {
-      return monster.name.toLowerCase().includes(searchField.toLowerCase())
-    })
-    return (
-      <div className="App">
-          
-          <SearchBox 
-             placeholder="search monsters" 
-             type="search"
-             onSearchHandler = {this.onSearchHandler}
-           />
-         <CardList monsters = {filteredMonster} />
-      </div>
-    );
+    render () {
+      return (
+        <div>
+          <HomePage />
+        </div>
+      )
+    }
+  
    }
    
     
  
-}
+
 
 export default App;
